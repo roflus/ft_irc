59091e41a60ce430ff7bf7548f638e58ec0c5f1a
Quilfort Frederiks <qfrederi@f1r6s2.codam.nl>
2023-07-27 12:14:36 +0200

    KICK Message working

--- include/Replies.hpp
+++ include/Replies.hpp
@@ -4,7 +4,7 @@
 #define MSG_WELCOME(user)   "001 " + user + " Welcome to our irc server"
 #define MSG_INVITING(user, target, channel) ":" + user + " INVITE " + target + " to channel :" + channel
 #define MSG_JOIN(user, channel)  ":" + user + " JOIN :" + channel
-#define MSG_KICK(user, target, channel, reason) ":" + user + " KICK " + target + " from :" + channel + " " + reason
+#define MSG_KICK(user, target, channel, reason) ":" + user + " KICK " + channel + " " + target + " :" + reason
 #define MSG_MODE(user, channel, modes) ":" + user + " " + channel + " " + modes
 #define MSG_NICK(user, nickname) ":" + user + " NICK " + " Set nickname to " + nickname
 #define MSG_USER(username) ":" + username + " :username is set" 
@@ -13,4 +13,5 @@
 #define RPL_TOPIC(channel, topic) "332 " + channel + " :" + topic
 #define RPL_CHANNELMODEIS(user, channel, modes) "324 " + user + " " + channel + " " + modes
 #define RPL_INVITING(channel , target) "341 " + channel + target  // returned by server to indicat that the attempted invite message was successful.
+
 #endif
